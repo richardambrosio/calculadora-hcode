046fdd7c2ff0ed1456a52d2eb76e2ce5c97f5040
Richard Ambrosio <richard.ambrosio1@live.com>
2021-10-26 15:49:21 -0300

    correções de bug com o ponto

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -36,7 +36,7 @@ class CalcController {
         this._operation = [];
         this._lastOperator = '';
         this._lastNumber = '';
-        
+
         this.setLastNumberToDisplay();
     }
 
@@ -148,7 +148,7 @@ class CalcController {
                 this.pushOperation(value)
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
             }
@@ -161,6 +161,8 @@ class CalcController {
 
     addDot() {
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');

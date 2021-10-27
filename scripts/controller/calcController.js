a0c62c602da6ea45be6f041560bf35a6af53a558
Richard Ambrosio <richard.ambrosio1@live.com>
2021-10-27 06:49:05 -0300

    Ctrl+C e Ctrl+V

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -15,6 +15,25 @@ class CalcController {
         this.initialize();
         this.initButtonEvents();
         this.initKeyboard();
+        this.pasteFromClipboard();
+    }
+
+    copyToClipboard() {
+        let input = document.createElement('input');
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand("Copy");
+        input.remove();
+    }
+
+    pasteFromClipboard() {
+        document.addEventListener('paste', e => {
+            let text = e.clipboardData.getData('Text');
+            
+            this.displayCalc = parseFloat(text);
+        });
     }
 
     initialize() {
@@ -63,6 +82,10 @@ class CalcController {
                 case '8':
                 case '9':
                     this.addOperation(parseInt(e.key));
+                    break;
+
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
                     break;
             }
         });
